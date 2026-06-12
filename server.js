4e3b9e446d4254e4bfe8760f543ef0b250d9059b
Richard Broodryk <rugbyanthemzone@outlook.com>
2026-06-12 09:04:42 +0200

    Fix Vite frontend CORS support

--- server.js
+++ server.js
@@ -68,8 +68,10 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://soccer-fan-zone.vercel.app"
     ],
-
     credentials: true,
   })
 );
